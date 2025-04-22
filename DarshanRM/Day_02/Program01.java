cc2b1345bffeb3d05129d569fbb5195efa1e3a4e
Darshan R M <darshanrm@zeta.tech>
2025-04-22 19:53:02 +0530

    Number star pattern

---
+++ DarshanRM/Day_02/Program01.java
@@ -0,0 +1,19 @@
+package org.darshan.Day2.FastestFingers;
+
+import java.util.Scanner;
+
+public class Program1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n - row; col++) {
+                System.out.print(n - row);
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
